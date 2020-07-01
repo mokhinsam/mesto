dfeeea471b9def4f164a680f4b297e422a46ac52
Darina Samokhina <mokhinsam@yandex.ru>
2020-07-01 18:04:05 +0300

    feat: добавить второй попап, подключить его в js

--- script/script.js
+++ script/script.js
@@ -1,39 +1,55 @@
-let popup = document.querySelector('.popup');
-let popupOpenButton = document.querySelector('.profile__open-popup');
-let popupCloseButton = popup.querySelector('.popup__close-button');
+const popupAdd  = document.querySelector('.popup__add');
+const popupEdit  = document.querySelector('.popup__edit');
+const popupEditOpenBtn = document.querySelector('.profile__edit-button');
+const popupAddOpenBtn = document.querySelector('.profile__add-button');
 
-let inputName = document.querySelector('.popup__field_name');
-let inputdAboutSelf = document.querySelector('.popup__field_about-self');
+const popupEditCloseBtn = document.querySelector('.popup__edit-close-btn');
+const popupAddCloseBtn = document.querySelector('.popup__add-close-btn');
 
-let profileName = document.querySelector('.profile__title');
-let profileAboutSelf = document.querySelector('.profile__subtitle');
+const inputName = document.querySelector('.popup__field_name');
+const inputdAboutSelf = document.querySelector('.popup__field_about-self');
+
+const profileName = document.querySelector('.profile__title');
+const profileAboutSelf = document.querySelector('.profile__subtitle');
 
 
-function popupToggle(event) {
+function popupToggle(popup) {
   popup.classList.toggle('popup_opened');
-  if (popup.classList.contains('popup_opened')) { 
-    inputName.value = profileName.textContent;
-    inputdAboutSelf.value = profileAboutSelf.textContent;
-  }
 }
 
-function clickInOverlay(event) {
+inputName.value = profileName.textContent;
+inputdAboutSelf.value = profileAboutSelf.textContent;
+
+function overlayClosedPopupAdd (event) {
   if (event.target === event.currentTarget) {
-    popupToggle();
+    popupToggle(popupAdd);
   }
 }
 
-popupOpenButton.addEventListener('click', popupToggle);
-popupCloseButton.addEventListener('click', popupToggle);
-popup.addEventListener('click', clickInOverlay);
+function overlayClosedPopupEdit (event) {
+  if (event.target === event.currentTarget) {
+    popupToggle(popupEdit);
+  }
+}
+
+popupEditOpenBtn.addEventListener('click', () => popupToggle(popupEdit));
+popupAddOpenBtn.addEventListener('click', () => popupToggle(popupAdd));
+
+popupEditCloseBtn.addEventListener('click', () => popupToggle(popupEdit));
+popupAddCloseBtn.addEventListener('click', () => popupToggle(popupAdd));
+
+popupAdd.addEventListener('click', overlayClosedPopupAdd);
+popupEdit.addEventListener('click', overlayClosedPopupEdit);
+
+
 
 let formElement = document.querySelector('.popup__container');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+function formSubmitHandler (event) {
+    event.preventDefault(); 
     profileName.textContent = inputName.value;
     profileAboutSelf.textContent = inputdAboutSelf.value;
-    popupToggle();
+    popupToggle(popupEdit);
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
